c5159f013d7eaa0a9fdfb07c8a2a3236612231dd
Kyle Oba <koba@mudphone.com>
2013-08-24 10:02:36 -1000

    Initial Commit

---
+++ FacesDemo/PDCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PDCAppDelegate.h
+//  FacesDemo
+//
+//  Created by koba on 8/24/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacesDemo/PDCViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDCViewController.h
+//  FacesDemo
+//
+//  Created by koba on 8/24/13.
+//  Copyright (c) 2013 Pas de Chocolat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCViewController : UIViewController
+
+@end
